cbf8491ecdcb11bd40e46966fac99a16ae0653a9
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2021-02-25 14:38:20 +0100

    :arrow_up: feat : list add prototype of function

--- list/List.hpp
+++ list/List.hpp
@@ -6,7 +6,7 @@
 /*   By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 11:46:10 by jereligi          #+#    #+#             */
-/*   Updated: 2021/02/22 12:38:57 by jereligi         ###   ########.fr       */
+/*   Updated: 2021/02/25 14:37:55 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,15 @@ namespace ft
 		*****             Iterators            *****
 		*******************************************/
 
+		iterator begin();
+		const_iterator begin() const;
+	    
+		iterator end();
+		const_iterator end() const;
+
+		reverse_iterator rend();
+		const_reverse_iterator rend() const;
+
 		/*******************************************
 		*****            Capacity              *****
 		*******************************************/
@@ -33,6 +42,7 @@ namespace ft
 		bool empty() const;
 		size_type size() const;
 		size_type max_size() const;
+		void resize (size_type n, value_type val = value_type());
 
 		/*******************************************
 		*****         Element access           *****
@@ -51,9 +61,47 @@ namespace ft
 		void assign (InputIterator first, InputIterator last);
 		void assign (size_type n, const value_type& val);
 
+		iterator insert (iterator position, const value_type& val);	
+    	void insert (iterator position, size_type n, const value_type& val);	
+		template <class InputIterator>
+    	void insert (iterator position, InputIterator first, InputIterator last);
 
+		iterator erase (iterator position);
+		iterator erase (iterator first, iterator last);
 
+		void push_back (const value_type& val);
+		void pop_back();
+		void push_front (const value_type& val);
+		void clear();
+		void swap (list& x);
+		
+		/*******************************************
+		*****       List operations            *****
+		*******************************************/
 
+		void splice (iterator position, list& x);	
+		void splice (iterator position, list& x, iterator i);	
+		void splice (iterator position, list& x, iterator first, iterator last);
+		
+		void remove (const value_type& val);
+		
+		template <class Predicate>
+  		void remove_if (Predicate pred);
+
+		void unique();
+		template <class BinaryPredicate>
+  		void unique (BinaryPredicate binary_pred);
+
+		void merge (list& x);
+		template <class Compare>
+		void merge (list& x, Compare comp);
+
+		void sort();
+		template <class Compare>
+		void sort (Compare comp);
+
+		void reverse();
+		
 	private:
 
 		/*Data Here*/;
